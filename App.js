28932822acb95b21d3ebe0ab0e1e6829c9ae81f0
ugurde98 <ugurde98@gmail.com>
2024-01-25 15:53:29 +0300

    initial

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView  style={{ flex: 1 }}>
+  <WebView
+      source={{ uri: 'https://teknokanyon.com/' }} // PWA'nızın adresi
+     
+    />
+    <StatusBar style="auto" />
+    </SafeAreaView>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
